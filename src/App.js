9bd2aed6173f0609065509b7e7c5928309e98c9d
NGHIA Hoang <nghia.hoang@ceresolutions.com>
2022-03-10 09:57:10 +0700

    init template-grid-3-column

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import './App.css';
+import ShopThreeColumn from './pages/ShopThreeColumn';
 
 function App() {
-  return <h1 className='text-red-500 text-center mt-6'>NGHIA Hoang</h1>;
+  return <ShopThreeColumn />;
 }
 
 export default App;
---
+++ src/pages/ShopThreeColumn.js
@@ -0,0 +1,7 @@
+function ShopThreeColumn() {
+  return (
+    <h1 className='text-red-600 text-center mt-6'>Template Grid 3 Column</h1>
+  );
+}
+
+export default ShopThreeColumn;
