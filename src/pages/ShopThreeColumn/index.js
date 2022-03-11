9940a8e5e37830edc8da6126c99003083eb94cc7
NGHIA Hoang <nghia.hoang@ceresolutions.com>
2022-03-11 16:19:50 +0700

    footer-section

--- src/pages/ShopThreeColumn/index.js
+++ src/pages/ShopThreeColumn/index.js
@@ -1519,6 +1519,33 @@ function ShopThreeColumn() {
         </div>
       </section>
       {/* ----- Contact section end ----- */}
+      {/* ----- Footer section start ----- */}
+      <footer id='footer' className='py-8 bg-gray-500'>
+        <div className='container'>
+          <div className='flex flex-wrap md:flex-row-reverse items-center justify-center gap-4'>
+            <div className='footer-image-container w-full md:w-1/2 text-center'>
+              <img
+                src='assets/images/logo/payment.webp'
+                alt=''
+                className='footer-image inline-block'
+                width='286'
+                height='23'
+              />
+            </div>
+            <div className='footer-content flex-1 flex items-center justify-center md:jutify-start'>
+              <p className='text-white flex flex-wrap items-center text-sm lg:text-base'>
+                © 2022 Sinp. Made with{' '}
+                <i className='icon-heart mx-2 text-orange'></i> by
+                <a href='#' className='ml-1 transition hover:text-orange'>
+                  Codecarnival
+                </a>
+                .
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
+      {/* ----- Footer section end ----- */}
     </>
   );
 }
